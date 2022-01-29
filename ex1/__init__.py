2c3a8c6960db41342e2205e5398ab46140d8b163
bagmarya <bagmarya@gmail.com>
2022-01-30 02:15:38 +0500

    решены все задачи

--- ex1/__init__.py
+++ ex1/__init__.py
@@ -3,5 +3,15 @@ def build_roles_tree(mapping):
     :param mapping: маппинг ролей в категории
     :return: дерево ролей
     """
-    # put your code here
-    pass
+    elements = []
+    for category_id in mapping["categoryIdsSorted"]:
+        id = "category-" + category_id
+        text = mapping["categories"][category_id]["name"]
+        items = [{"id": i, "text": mapping["roles"][i]["name"]} for i in mapping["categories"][category_id]["roleIds"]]
+        element = {"id": id, "text": text, "items": items}
+        elements.append(element)
+
+    return {'categories': elements}
+
+
+
--- ex2/__init__.py
+++ ex2/__init__.py
@@ -1,3 +1,5 @@
+import time
+
 from ex2 import fetcher
 
 CALL_COUNT = 10
@@ -11,11 +13,22 @@ def benchmark(num):
     :return: функцию обёртку
     """
     def wrapper(func):
-        # put your code here
-        pass
+        def wrapped(*args, **kwargs):
+            total = 0
+            for _ in range(num):
+                start = time.time()
+                func(*args, **kwargs)
+                end = time.time()
+                print(f'Время выполнения: {end - start}')
+                total += (end - start)
+            print(f'Среднее время выполнения: {total / num}')
+        return wrapped
     return wrapper
 
 
 @benchmark(CALL_COUNT)
 def fetch_page(url):
     fetcher.get(url)
+
+if __name__ == "__main__":
+    fetch_page('https://google.com')
